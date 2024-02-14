304b6701f7c999ea16f23ee3d67c1ec411a40298
Ale Fernandez <alejandro.d.fernandez.88@gmail.com>
2024-02-14 20:40:32 -0300

    Corrigiendo erroes 1

--- ejercicioClase3.py
+++ ejercicioClase3.py
@@ -1,23 +1,23 @@
-# Función para sumar dos números
+# Función para sumar 
 def sumar(a, b):
     return a + b
 
-# Función para restar dos números
+# Función para restar 
 def restar(a, b):
     return a - b
 
-# Función para multiplicar dos números
+# Función para multiplicar 
 def multiplicar(a, b):
     return a * b
 
-# Función para dividir dos números
+# Función para dividir 
 def dividir(a, b):
     if b != 0:
         return a / b
     else:
         return "No se puede dividir por cero."
 
-# Solicitar al usuario que elija una opción
+# Pedir al usuario que elija una opción
 print("Seleccione una opción:")
 print("1. Sumar")
 print("2. Restar")
@@ -25,11 +25,11 @@ print("3. Multiplicar")
 print("4. Dividir")
 opcion = int(input("Ingrese el número de la opción deseada: "))
 
-# Solicitar al usuario que ingrese dos números
+# Pedir al usuario que ingrese dos números
 num1 = float(input("Ingrese el primer número: "))
 num2 = float(input("Ingrese el segundo número: "))
 
-# Realizar la operación seleccionada y mostrar el resultado por consola
+# Realizar la operación seleccionada e imprimir consola
 if opcion == 1:
     print("La suma es:", sumar(num1, num2))
 elif opcion == 2:
